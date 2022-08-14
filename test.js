d14719695d88d971c60bf62be41a4dd19b05608f
DevUmutYildirim <devumutyildirim@gmail.com>
2022-08-14 05:36:04 +0300

    Mongoose CRUD added

---
+++ test.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
+
+// create schema
+const PhotoSchema = new Schema({
+    title: String,
+    description: String
+})
+
+// create a model and photo collection using PhotoSchema
+const Photo = mongoose.model('Photo', PhotoSchema)
+
+// create a photo
+const createPhoto = Photo.create({
+    title: "Photo Title 2",
+    description: "Photo Description 2 lorem ipsum"
+})
+
+// read a photo
+const readPhoto = Photo.find({}, (err, data) => {
+    console.log(data);
+})
+
+// update a photo
+const id = "";
+
+const updatePhoto = Photo.findByIdAndUpdate(
+    id, {
+        title: "Photo title 111 updated",
+        description: "Photo description 1 updated"
+    },
+    {
+        new: true
+    },
+    (err, data) => {
+        console.log(data);
+    }
+)
+
+// delete a photo
+const id2 = "";
+const deletePhoto = Photo.findByIdAndDelete(id2, (err, data) => {
+    console.log("Photo is removed");
+})
