5ab9b467080b8ee1cbc397fa8c2fde9ffcf28e2f
DevUmutYildirim <devumutyildirim@gmail.com>
2022-08-15 05:25:51 +0300

    added adding and sorting photos

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./Models/Photo');
 
 const app = express();
 
-// if we dont type next(); , function doenst continue to another middleware function. 
-// next function is like the break function of if ef/else, while, switch case loops.
-const myLogger = (req, res, next) => {
-    console.log("Middleware log 1");
-    next();
-}
-
-const myLogger2 = (req, res, next) => {
-    console.log("Middleware log 2");
-    next();
-}
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+});
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static('public'))
-app.use(myLogger);
-app.use(myLogger2);
+app.use(express.urlencoded({extended: true})) // read the data from the url
+app.use(express.json()) // convert the data to JSON
 
 // ROUTES
-// also app.get and root are middleware functions. 
-app.get('/', (req, res) => {
-    res.render("index");
-})
+// app.get and root are middleware functions. 
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({}) // find all Photos from the database
+    res.render('index', {               // post the index template engine
+        photos
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render("about");
-})
+});
 app.get('/add', (req, res) => {
     res.render("add");
-})
+});
+// take the request body and post to the pcat-test-db.photos collection
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/')
+});
+
 
 
 const port = 3000;
 app.listen(port, () => {
-    console.log('Server is started on ' + port + 'port');
+    console.log('Server is started on ' + port + ' port');
 })
--- test.js
+++ test.js
@@ -1,50 +1,58 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
 // connect DB
-mongoose.connect('mongodb://localhost/pcat-test-db', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-});
+// mongoose.connect('mongodb://localhost/pcat-test-db', {
+//     useNewUrlParser : true,
+//     useUnifiedTopology : true
+// });
 
 // create schema
-const PhotoSchema = new Schema({
-    title: String,
-    description: String
-})
+// const PhotoSchema = new Schema({
+//     title: String,
+//     description: String
+// })
 
 // create a model and photo collection using PhotoSchema
-const Photo = mongoose.model('Photo', PhotoSchema)
+// const Photo = mongoose.model('Photo', PhotoSchema)
 
 // create a photo
-const createPhoto = Photo.create({
-    title: "Photo Title 2",
-    description: "Photo Description 2 lorem ipsum"
-})
+// const createPhoto = Photo.create({
+//     title: "User's Photos",
+//     description: "My Description"
+// })
 
 // read a photo
-const readPhoto = Photo.find({}, (err, data) => {
-    console.log(data);
-})
+// const readPhoto = Photo.find({}, (err, data) => {
+//     console.log(data);
+// })
 
 // update a photo
-const id = "";
+// const id = "";
 
-const updatePhoto = Photo.findByIdAndUpdate(
-    id, {
-        title: "Photo title 111 updated",
-        description: "Photo description 1 updated"
-    },
-    {
-        new: true
-    },
-    (err, data) => {
-        console.log(data);
-    }
-)
+// const updatePhoto = Photo.findByIdAndUpdate(
+//     id, {
+//         title: "Photo title 111 updated",
+//         description: "Photo description 1 updated"
+//     },
+//     {
+//         new: true
+//     },
+//     (err, data) => {
+//         console.log(data);
+//     }
+// )
 
 // delete a photo
-const id2 = "";
-const deletePhoto = Photo.findByIdAndDelete(id2, (err, data) => {
-    console.log("Photo is removed");
-})
+// const id2 = "";
+// const deletePhoto = Photo.findByIdAndDelete(id2, (err, data) => {
+//     console.log("Photo is removed");
+// })
+
+
+// if we dont type next(); , function doenst continue to another middleware function. 
+// next function is like the break function of if ef/else, while, switch case loops.
+// const myLogger = (req, res, next) => {
+//     console.log("Middleware log 1");
+//     next();
+// }
